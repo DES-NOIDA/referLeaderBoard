3520424e56a7e163baa6f3ab0187d6d000c7a674
Singh Gaurav <ctgaurav@gmail.com>
2022-03-30 09:50:11 +0530

    Initial

---
+++ src/test/java/com/thales/ReferralBoard/ReferralBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thales.ReferralBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferralBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
